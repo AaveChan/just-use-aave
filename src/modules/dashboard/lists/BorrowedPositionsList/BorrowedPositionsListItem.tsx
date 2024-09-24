8a50424de9dbaa25b628bd1805dc3736aee9de51
MartinGbz <martin.grabarz@outlook.fr>
2024-09-24 13:07:35 +0200

    ✨ implement LSTs native yield (#3)

--- src/modules/dashboard/lists/BorrowedPositionsList/BorrowedPositionsListItem.tsx
+++ src/modules/dashboard/lists/BorrowedPositionsList/BorrowedPositionsListItem.tsx
@@ -14,6 +14,7 @@ import { showSuperFestTooltip, Side } from 'src/utils/utils';
 
 import { ListColumn } from '../../../../components/lists/ListColumn';
 import { ListAPRColumn } from '../ListAPRColumn';
+import { ListAPYDetails } from '../ListAPYDetails';
 import { ListButtonsColumn } from '../ListButtonsColumn';
 import { ListItemAPYButton } from '../ListItemAPYButton';
 import { ListItemWrapper } from '../ListItemWrapper';
@@ -144,7 +145,16 @@ const BorrowedPositionsListItemDesktop = ({
     >
       <ListValueColumn symbol={reserve.symbol} value={totalBorrows} subValue={totalBorrowsUSD} />
 
-      <ListAPRColumn value={borrowAPY} incentives={incentives} symbol={reserve.symbol} />
+      <ListAPRColumn
+        value={reserve.underlyingAPY ? borrowAPY + reserve.underlyingAPY : borrowAPY}
+        tooltip={
+          reserve.underlyingAPY ? (
+            <ListAPYDetails borrowAPY={borrowAPY} underlyingAPY={reserve.underlyingAPY} />
+          ) : null
+        }
+        incentives={incentives}
+        symbol={reserve.symbol}
+      />
 
       <ListColumn>
         <ListItemAPYButton
@@ -234,7 +244,12 @@ const BorrowedPositionsListItemMobile = ({
 
       <Row caption={<Trans>APY</Trans>} align="flex-start" captionVariant="description" mb={2}>
         <IncentivesCard
-          value={borrowAPY}
+          value={reserve.underlyingAPY ? borrowAPY + reserve.underlyingAPY : borrowAPY}
+          tooltip={
+            reserve.underlyingAPY ? (
+              <ListAPYDetails borrowAPY={borrowAPY} underlyingAPY={reserve.underlyingAPY} />
+            ) : null
+          }
           incentives={incentives}
           symbol={symbol}
           variant="secondary14"
